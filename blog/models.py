632ca9d2b2c3d17426b2f341e869ff317e5d9258
Tim Paetz <tim@cadmiumcd.com>
2017-05-10 15:21:32 -0400

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+from django.urls import reverse
+from django.utils import timezone
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+    def populate_posts(self):
+        for i in range(20):
+            post = Post()
+            post.title = 'Populated Post - {}'.format(i)
+            post.slug = 'populated-post-{}'.format(i)
+            post.author_id = 1
+            post.body = 'Body of populated post {}'.format(i)
+            post.status = 'published'
+            post.save()
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=25, choices=STATUS_CHOICES, default='draft')
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.strftime('%m'),
+                                                 self.publish.strftime('%d'),
+                                                 self.slug])
---
+++ blog/views.py
@@ -0,0 +1,34 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+from django.views.generic import ListView
+
+from blog.models import Post
+
+
+def post_list(request):
+    all_posts = Post.published.all()
+
+    paginator = Paginator(all_posts, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'post/list.html', {'posts': posts})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'post/list.html'
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
+    return render(request, 'post/detail.html', {'post': post})
