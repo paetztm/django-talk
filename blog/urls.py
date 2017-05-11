043ba2a9baec940223b5b028cd5126bd89ee01bd
paetztm <tim@cadmiumcd.com>
2017-05-11 14:53:07 -0400

    Added translations, sharing and emails

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from blog import views
 
@@ -7,4 +8,6 @@ urlpatterns = [
     url(r'^$', views.PostListView.as_view(), name='post_list'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$',
         views.post_detail, name='post_detail'),
+    url(r'^translations/$', TemplateView.as_view(template_name='post/translations.html')),
+    url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
+from django.contrib import messages
+from django.core.mail import send_mail
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
-
-# Create your views here.
+from django.utils.translation import ugettext_lazy as _
 from django.views.generic import ListView
 
+from blog.forms import EmailPostForm
 from blog.models import Post
 
 
@@ -32,3 +34,31 @@ class PostListView(ListView):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
     return render(request, 'post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+            if cd['to'] == 'admin@myblog.com':
+                messages.error(request, _('Please fill out the form and resubmit.'))
+            else:
+                send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+                sent = True
+
+    else:
+        form = EmailPostForm()
+    return render(request, 'post/share.html', {'post': post,
+                                               'form': form,
+                                               'sent': sent})
+
