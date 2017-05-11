4719e58a0627cbd86c785248f14d1047337b4cfa
paetztm <tim@cadmiumcd.com>
2017-05-11 14:18:02 -0400

    Implemented tests

--- polls/urls.py
+++ polls/urls.py
@@ -4,9 +4,11 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    #url(r'^$', views.index, name='index'),
     # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    #url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
